34bba8a445bc068c73d894e91967bf60ff921577
reamd7 <admin@admin.cn>
2019-02-11 22:47:46 +0800

    yarn add babel-loader babel-preset-react-app @babel/core browserlistrc

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -2,12 +2,24 @@ import path from "./path";
 import webpack from "webpack";
 
 const config: webpack.Configuration = {
-    mode:"development",
-    entry:path.appIndexJs,
-    output:{
-        path:path.appBuild,
-        filename:"index.bundle.js"
+  mode: "development",
+  entry: path.appIndexJs,
+  output: {
+    path: path.appBuild,
+    filename: "index.bundle.js",
+    publicPath: "/",
+    chunkFilename: "js/[name].js"
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx"],
+    alias: {
+      react: "anujs/dist/ReactIE.js",
+      "react-dom": "anujs/dist/ReactIE.js",
+      "prop-types": "anujs/lib/ReactPropTypes",
+      devtools: "anujs/lib/devtools",
+      "create-react-class": "anujs/lib/createClass"
     }
+  }
 };
 
 export default config;
